bd58bdedf1aa02678890d0cfdf3026c8f1a9dac3
datastream <guxianjie@gmail.com>
2013-09-13 19:04:35 +0800

    remove localstorage

--- js/collections/hostmetrics.js
+++ js/collections/hostmetrics.js
@@ -7,7 +7,6 @@ var app = app || {};
     var HostMetrics = Backbone.Collection.extend({
         model: app.HostMetric,
         url_api: '/api/v1/host/',
-        localStorage: new Backbone.LocalStorage('todos-backbone'),
     });
     app.hostmetrics = new HostMetrics();
 })();
